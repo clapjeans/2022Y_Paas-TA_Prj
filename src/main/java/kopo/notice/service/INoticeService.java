00f0a698faffec48247da3c74a7bccf7c3d4b506
choi <me940728@naver.com>
2022-02-12 15:39:07 +0900

    [CREATE] First Paas_TA FrameWork

---
+++ src/main/java/kopo/notice/service/INoticeService.java
@@ -0,0 +1,22 @@
+package kopo.notice.service;
+
+import kopo.notice.dto.NoticeDTO;
+
+import java.util.List;
+
+
+public interface INoticeService {
+
+	List<NoticeDTO> getNoticeList() throws Exception;
+	
+	void InsertNoticeInfo(NoticeDTO pDTO) throws Exception;
+
+	NoticeDTO getNoticeInfo(NoticeDTO pDTO) throws Exception;
+	
+	void updateNoticeReadCnt(NoticeDTO pDTO) throws Exception;
+	
+	void updateNoticeInfo(NoticeDTO pDTO) throws Exception;
+	
+	void deleteNoticeInfo(NoticeDTO pDTO) throws Exception;
+	
+}
